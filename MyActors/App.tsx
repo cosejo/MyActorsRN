cf700633b328fcfb15ac270fa92be28401708233
Carlos Osejo <caosejo@gmail.com>
2021-07-23 00:24:56 -0600

    Add Copilot example

--- MyActors/App.tsx
+++ MyActors/App.tsx
@@ -8,108 +8,120 @@
  * @format
  */
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, Image, View, TouchableOpacity, Switch, Button } from 'react-native';
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+import { copilot, walkthroughable, CopilotStep } from 'react-native-copilot';
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+const WalkthroughableText = walkthroughable(Text);
+const customSvgPath = ({ position, size, canvasSize, step }): string => {
+  return `M0,0H${canvasSize.x}V${canvasSize.y}H0V0ZM${position.x._value},${position.y._value + 50}H${position.x._value + size.x._value}V${position.y._value + size.y._value}H${position.x._value}V${position.y._value}Z`;
+};
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  middleView: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  labelText: {
+    fontSize: 18,
+    color: '#2980b9',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#2980b9',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  tooltip: {
+    backgroundColor: "#9FA8DA",
+    borderRadius: 10,
+    paddingTop: 5
+  }
+});
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+class App extends Component {
 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+  static propTypes = {
+    start: PropTypes.func.isRequired
+  };
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
+  componentDidMount() {
+    this.props.start()
+  }
 
- export default App;
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.middleView}>
+          <CopilotStep text="Tutorial para usar texto 1" order={1} name="firstText">
+            <WalkthroughableText style={styles.labelText}>
+              {'Primer Texto'}
+            </WalkthroughableText>
+          </CopilotStep>  
+          <CopilotStep text="Tutorial para usacr botón 1" order={2} name="firstButton">
+            <TouchableOpacity style={styles.button}>
+              <WalkthroughableText style={styles.buttonText}>
+                {'Primer Botón'}
+              </WalkthroughableText>
+            </TouchableOpacity>
+          </CopilotStep>
+        </View>
+        <View style={styles.middleView}>
+          <CopilotStep text="Tutorial para usar texto 2" order={3} name="secondText">
+            <WalkthroughableText style={styles.labelText}>
+              {'Segundo Texto'}
+            </WalkthroughableText>
+          </CopilotStep>
+          <CopilotStep text="Tutorial para usar texto 2" order={4} name="secondButton">
+            <TouchableOpacity style={styles.button}>
+              <WalkthroughableText style={styles.buttonText}>
+                {'Segundo Botón'}
+              </WalkthroughableText>
+            </TouchableOpacity>
+          </CopilotStep>
+        </View>
+        <View style={styles.middleView}>
+          <CopilotStep text="Tutorial para usar texto 3" order={5} name="thridText">
+            <WalkthroughableText style={styles.labelText}>
+              {'Tercer Texto'}
+            </WalkthroughableText>
+          </CopilotStep>  
+          <CopilotStep text="Tutorial para usar botón 3" order={6} name="thirdButton">
+            <TouchableOpacity style={styles.button}>
+              <WalkthroughableText style={styles.buttonText}>
+                {'Tercer Botón'}
+              </WalkthroughableText>
+            </TouchableOpacity>
+          </CopilotStep>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default copilot({
+  backdropColor: "rgba(0, 0, 0, 0.4)",
+  animated: false, // Can be true or false
+  overlay: 'svg', // Can be either view or svg
+  svgMaskPath: customSvgPath
+})(App);
